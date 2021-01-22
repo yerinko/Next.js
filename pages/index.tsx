0531a2b56a35703fc6418e691dc1ac8c2c7e0047
고매니저 <yerinko@moditt.com>
2021-01-22 12:19:35 +0900

    Next.js + Typescript 초기 설정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,34 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styled from "styled-components";
+
 
 export default function Home() {
-  return <div>Welcome to Next.js!</div>
-}
+  return (
+      <HomeBlock>
+        <Head>
+          <title>Create Next App</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Mains>
+          <h1>Next.js + Typescript</h1>
+        </Mains>
+      </HomeBlock>
+  );
+};
+
+const HomeBlock = styled.div`
+  min-height: 100vh;
+  padding: 0 0.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+const Mains = styled.div`
+  min-height: 100vh;
+  padding: 0 0.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
